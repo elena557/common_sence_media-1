e6e7111fb2b5aa4c000c3eb6c426cfe12795b475
Dima Vainarouski <dima.vainarouski@macys.com>
2014-04-10 02:44:56 -0700

    Updated report and handaling errors exeption

--- reviews_test/lib/xml_element_validation.rb
+++ reviews_test/lib/xml_element_validation.rb
@@ -2,6 +2,8 @@ require 'nokogiri'
 require 'trollop'
 require 'open-uri'
 require_relative '../common/test_spec'
+require_relative '../common/custom_error'
+
 
 $MODULE_DIR = File.expand_path(File.dirname(__FILE__)).gsub(/lib/, '')
 
@@ -33,7 +35,7 @@ class XmlValidator
     @test_start_time = Time.now
 
     puts "Test started at #{@test_start_time}"
-    puts "Progress: "
+    puts "\nProgress: "
 
     @log += <<-EOR
 Test started at #{@test_start_time}
@@ -71,9 +73,6 @@ XML is loaded
 
     report_writer(run_test(test_document, test_spec))
 
-    @test_end_time = Time.now
-    @test_duration = @test_end_time - @test_start_time
-
     @log += <<-EOR
 
 Test finished at #{@test_end_time}
@@ -88,63 +87,69 @@ Test duration: #{@test_duration} sec
 
   def xml_body(channel)
     begin
-      if @input_file.nil?
-        if @limit == '0'
-          uri = "http://api.#{@domain}/api/v2/reviews/browse?api_key=#{@api_key}&channel=#{channel.chomp}"
-        else
-          uri = "http://api.#{@domain}/api/v2/reviews/browse?api_key=#{@api_key}&channel=#{channel.chomp}&limit=#{@limit}"
-        end
-
-        @log += <<-EOR
-URL for xml file is: #{uri}
-        EOR
-
-        xml_body = Nokogiri::XML(open(uri))
+      if @limit == '0'
+        uri = "http://api.#{@domain}/api/v2/reviews/browse?api_key=#{@api_key}&channel=#{channel.chomp}"
       else
-
-        @log += <<-EOR
-Filename of xml file is: #{@input_file}
-        EOR
-
-        xml_body = Nokogiri::XML(File.open(@input_file))
+        uri = "http://api.#{@domain}/api/v2/reviews/browse?api_key=#{@api_key}&channel=#{channel.chomp}&limit=#{@limit}"
       end
+
+      @log += <<-EOR
+URL for xml file is: #{uri}
+      EOR
+
+      xml_body = Nokogiri::XML(open(uri))
+
     rescue Exception => e
 
       @log += <<-EOR
-URL or filename is incorrect: #{e.message}
+URL is incorrect: #{e.message}
       EOR
+
+      save_log
+      raise "URL or filename is incorrect: #{e.message}"
     end
     xml_body
   end
 
 
   def test_status_analyzer(test_result)
-    result = OpenStruct.new
+    begin
+      result = OpenStruct.new
 
-    test_statuses = []
+      test_statuses = []
 
-    unless test_result.test_of_value.nil?
-      test_statuses << test_result.test_of_value.test_status
-    end
+      unless test_result.test_of_value.nil?
+        test_statuses << test_result.test_of_value.test_status
+      end
 
-    unless test_result.test_of_attributes.nil?
-      test_statuses << test_result.test_of_attributes.test_status
-    end
+      unless test_result.test_of_attributes.nil?
+        test_statuses << test_result.test_of_attributes.test_status
+      end
 
-    unless test_result.test_of_child_elements.nil?
-      test_statuses << test_result.test_of_child_elements.test_status
-    end
+      unless test_result.test_of_child_elements.nil?
+        test_statuses << test_result.test_of_child_elements.test_status
+      end
 
-    if test_statuses.include?("FAIL")
-      result.test_status = "FAIL"
-    else
-      result.test_status = "PASS"
-    end
+      if test_statuses.include?("FAIL")
+        result.test_status = "FAIL"
+      else
+        result.test_status = "PASS"
+      end
 
-    @log += <<-EOR
+      @log += <<-EOR
 
 Test of element summary result: #{result.test_status}
-    EOR
+      EOR
+
+    rescue Exception => e
+
+      @log += <<-EOR
+Unexpected error in test_status_analyzer method, error: #{e.message}
+      EOR
+
+      save_log
+      raise XmlValidatorMethodError, "Unexpected error in test_status_analyzer method, error: #{e.message}" unless e.include?('method, error:')
+    end
 
     result
   end
@@ -232,10 +237,11 @@ Test of attribute, Test message: #{test_attribute.test_message}
     rescue Exception => e
 
       @log += <<-EOR
-Unexpected error in attribute validation method, error: #{e.message}
+Unexpected error in attribute_verification method, error: #{e.message}
       EOR
 
       save_log
+      raise XmlValidatorMethodError, "Unexpected error in attribute_verification method, error: #{e.message}"
     end
 
     test_results
@@ -243,14 +249,14 @@ Unexpected error in attribute validation method, error: #{e.message}
 
 
   def value_validation(test_element, expected_value)
+    begin
 
-    @log += <<-EOR
+      @log += <<-EOR
 
 Value validation, Actual value: #{test_element.text}
 Value validation, Expected value: #{expected_value}
-    EOR
+      EOR
 
-    begin
       test_of_value = OpenStruct.new
 
       if expected_value == "*"
@@ -282,10 +288,11 @@ Value validation, Test message: #{test_of_value.test_message}
     rescue Exception => e
 
       @log += <<-EOR
-Unexpected error in value validation method, error: #{e.message}
+Unexpected error in value_validation method, error: #{e.message}
       EOR
 
       save_log
+      raise XmlValidatorMethodError, "Unexpected error in value_validation method, error: #{e.message}"
     end
 
     test_of_value
@@ -293,86 +300,113 @@ Unexpected error in value validation method, error: #{e.message}
 
 
   def child_elements_validation(test_element, expected_child_elements)
-
-    @log += <<-EOR
+    begin
+      @log += <<-EOR
 
 Child element validation:
 #{"*"*120}
-    EOR
+      EOR
 
-    test_results = OpenStruct.new
-    test_results.child_elements = test_of_elements(test_element, expected_child_elements)
-
-
-    if test_results.child_elements.any? { |child_element| child_element.test_status == "FAIL" }
-      test_results.test_status = "FAIL"
-    else
-      test_results.test_status = "PASS"
-    end
+      test_results = OpenStruct.new
+      test_results.child_elements = test_of_elements(test_element, expected_child_elements)
 
 
-    @log += <<-EOR
+      if test_results.child_elements.any? { |child_element| child_element.test_status == "FAIL" }
+        test_results.test_status = "FAIL"
+      else
+        test_results.test_status = "PASS"
+      end
+
+
+      @log += <<-EOR
 
 Child element validation, Test result: #{test_results.test_status}
 #{"*"*120}
-    EOR
+      EOR
+
+    rescue XmlValidatorMethodError => ex
+      raise XmlValidatorMethodError, ex.message
+
+    rescue Exception => e
+
+      @log += <<-EOR
+Unexpected error in child_elements_validation method, error: #{e.message}
+      EOR
+
+      save_log
+      raise XmlValidatorMethodError, "Unexpected error in child_elements_validation method, error: #{e.message}" unless e.message.include?('method, error:')
+    end
 
     test_results
   end
 
 
   def test_of_elements(test_node, test_elements)
+    begin
 
-    elements = []
+      elements = []
 
-    test_elements.each do |element|
+      test_elements.each do |element|
 
-      text_message = " Test element id: #{element["element_id"]}, Test element name: #{element["element_name"]} "
-      boarder = "-"*((120 - text_message.size)/2)
-
-      @log += <<-EOR
-
-#{ boarder + text_message + boarder }
-      EOR
-
-      element_test_result = OpenStruct.new
-      element_test_result.id = element["element_id"]
-      element_test_result.name = element["element_name"]
-
-      test_element =  test_node.css(element["element_name"].gsub(":","|"))
-
-      # Verifying element existence
-      if test_element.to_s == ""
-        element_test_result.test_status = "FAIL"
-        element_test_result.test_message = "Element not exist!"
+        text_message = " Test element id: #{element["element_id"]}, Test element name: #{element["element_name"]} "
+        boarder = "-"*((120 - text_message.size)/2)
 
         @log += <<-EOR
+
+#{ boarder + text_message + boarder }
+        EOR
+
+        element_test_result = OpenStruct.new
+        element_test_result.id = element["element_id"]
+        element_test_result.name = element["element_name"]
+
+        test_element =  test_node.css(element["element_name"].gsub(":","|"))
+
+        # Verifying element existence
+        if test_element.to_s == ""
+          element_test_result.test_status = "FAIL"
+          element_test_result.test_message = "Element not exist!"
+
+          @log += <<-EOR
 Test of element existence, Element not exist
 
 Test of element summary result: #{ element_test_result.test_status }
-        EOR
+          EOR
+
+          elements << element_test_result
+          next
+        end
+
+
+        # Verifying element value
+        element_test_result.test_of_value = value_validation(test_element, element['element_value']) unless element['element_value'].nil?
+
+        # Verifying element attributes
+        element_test_result.test_of_attributes = attribute_verification(test_element, element['attributes']) unless element['attributes'].nil?
+
+        # Verifying child elements
+        element_test_result.test_of_child_elements = child_elements_validation(test_element, element['child_elements']) unless element['child_elements'].nil?
+
+
+        analyzed_results = test_status_analyzer(element_test_result)
+        element_test_result.test_status = analyzed_results.test_status
+        element_test_result.test_message = analyzed_results.test_message
+
 
         elements << element_test_result
-        next
       end
 
+    rescue XmlValidatorMethodError => ex
+      raise XmlValidatorMethodError, ex.message
 
-      # Verifying element value
-      element_test_result.test_of_value = value_validation(test_element, element['element_value']) unless element['element_value'].nil?
+    rescue Exception => e
 
-      # Verifying element attributes
-      element_test_result.test_of_attributes = attribute_verification(test_element, element['attributes']) unless element['attributes'].nil?
+      @log += <<-EOR
+Unexpected error in test_of_elements method, error: #{e.message}
+      EOR
 
-      # Verifying child elements
-      element_test_result.test_of_child_elements = child_elements_validation(test_element, element['child_elements']) unless element['child_elements'].nil?
-
-
-      analyzed_results = test_status_analyzer(element_test_result)
-      element_test_result.test_status = analyzed_results.test_status
-      element_test_result.test_message = analyzed_results.test_message
-
-
-      elements << element_test_result
+      save_log
+      raise XmlValidatorMethodError, "Unexpected error in test_of_elements method, error: #{e.message}"
     end
 
     elements
@@ -380,63 +414,93 @@ Test of element summary result: #{ element_test_result.test_status }
 
 
   def run_test(test_document, test_spec)
+    begin
 
-    all_nodes = test_document.css(test_spec["container"].gsub(":","|"))
+      all_nodes = test_document.css(test_spec["container"].gsub(":","|"))
 
-    if  all_nodes.empty?
+      if  all_nodes.empty?
 
-      @log += <<-EOR
+        @log += <<-EOR
 Unable to locate container element '#{test_spec["container"]}'
-      EOR
+        EOR
 
-      raise "Unable to locate container element"
-    end
+        raise "Unable to locate container element"
+      end
 
-    all_nodes.map do |container|
-      test_node = OpenStruct.new
-      test_node.id = (/\d+/.match container.search('id').text).to_s
+      results = all_nodes.map do |container|
+        test_node = OpenStruct.new
+        test_node.id = (/\d+/.match container.search('id').text).to_s
 
-      print "#"
+        print "#"
 
-      @log += <<-EOR
+        @log += <<-EOR
 #{"="*120}
 Test element id is #{test_node.id}
 #{"="*120}
+        EOR
+
+        test_node.elements = test_of_elements(container, test_spec['elements'])
+        test_node
+      end
+
+      @number_elements = results.size
+      @log += <<-EOR
+
+Number of elements tested: #{@number_elements}
       EOR
 
-      test_node.elements = test_of_elements(container, test_spec['elements'])
-      test_node
+      puts "\nNumber of elements tested: #{@number_elements}"
+
+    rescue XmlValidatorMethodError => ex
+      raise XmlValidatorMethodError, ex.message
+
+    rescue Exception => e
+
+      @log += <<-EOR
+Unexpected error in run_test method, error: #{e.message}
+      EOR
+
+    save_log
+    raise  "Unexpected error in run_test method, error: #{e.message}"
     end
+
+    results
   end
 
 
   def report_writer(test_report)
     filename = %{#{Time.now.strftime('%Y_%m_%d_%H_%M')}_#{@channel}_channel.txt}
     File.open("#{@output_folder}/#{filename}", "w") do |f|
+      f.puts "Test start time: #{@test_start_time}"
       test_report.each do |node|
         f.puts("="*120)
         f.puts "Tested element ID: #{node.id}"
         f.puts("="*120)
         node['elements'].each do |element|
-          f.puts("Element ##{element.id}: <#{element.name}>") if element.test_status == "FAIL"
+
+          if element.test_status == "FAIL"
+            f.puts("")
+            text_message = " Test element id: #{element.id}, Test element name: #{element.name} "
+            boarder = "-"*((120 - text_message.size)/2)
+            f.puts boarder + text_message + boarder
+          end
 
           if !element.test_of_value.nil? && element.test_of_value.test_status == "FAIL"
             f.puts("")
-            f.puts("Element value validation")
-            f.puts("#{element.test_of_value.test_status} - #{element.test_of_value.test_message}")
-            f.puts("-"*120)
+            f.puts("Test of element value, Test status: #{element.test_of_value.test_status}")
+            f.puts("Test of element value, Test message: #{element.test_of_value.test_message}")
           end
 
           if !element.test_of_attributes.nil? && element.test_of_attributes.test_status == "FAIL"
-            f.puts("")
-            f.puts("Attribute validation:")
             failed_attribues = element.test_of_attributes.attributes.select { |attr| attr.test_status ==  "FAIL" }
             failed_attribues.each do |attribute|
-              f.puts("#{attribute.test_status} - Attribute ##{attribute.id}: #{attribute.name}")
-              f.puts("Expected value: #{attribute.expected_value}")
-              f.puts("Actual value: #{attribute.actual_value}")
-              f.puts("Test message: #{attribute.test_message}")
-              f.puts("-"*120)
+              f.puts("")
+              f.puts("Test of attribute, Attribute id ##{attribute.id}")
+              f.puts("Test of attribute, Attribute name: #{attribute.name}")
+              f.puts("Test of attribute, Expected value: #{attribute.expected_value}")
+              f.puts("Test of attribute, Actual value: #{attribute.actual_value}")
+              f.puts("Test of attribute, Test status: #{attribute.test_status}")
+              f.puts("Test of attribute, Test message: #{attribute.test_message}")
             end
           end
 
@@ -444,37 +508,57 @@ Test element id is #{test_node.id}
           if !element.test_of_child_elements.nil? && element.test_of_child_elements.test_status == "FAIL"
             f.puts("")
             f.puts("Child elements validation:")
+            f.puts("*"*120)
             element.test_of_child_elements.child_elements.each do |child_element|
 
-              f.puts("Child element ##{child_element.id}: <#{child_element.name}>") if child_element.test_status == "FAIL"
+              if child_element.test_status == "FAIL"
+                text_message = " Child element id: #{child_element.id}, name: <#{child_element.name}> "
+                boarder = "-"*((120 - text_message.size)/2)
+                f.puts boarder + text_message + boarder
+              end
+
 
               if !child_element.test_of_value.nil? && child_element.test_of_value.test_status == "FAIL"
                 f.puts("")
-                f.puts("Chield element value validation")
-                f.puts("#{child_element.test_of_value.test_status} - #{child_element.test_of_value.test_message}")
-                f.puts("-"*120)
+                f.puts("Child element test of value, Test status: #{child_element.test_of_value.test_status}")
+                f.puts("Child element test of value, Test message: #{child_element.test_of_value.test_message}")
               end
 
 
               if !child_element.test_of_attributes.nil? && child_element.test_of_attributes.test_status == "FAIL"
                 f.puts("")
-                f.puts("Child element attribute validation:")
                 failed_attribues = child_element.test_of_attributes.attributes.select { |attr| attr.test_status ==  "FAIL" }
                 failed_attribues.each do |attribute|
-                  f.puts("#{attribute.test_status} - Child attribute ##{attribute.id}: #{attribute.name}")
-                  f.puts("Expected value: #{attribute.expected_value}")
-                  f.puts("Actual value: #{attribute.actual_value}")
-                  f.puts("Test message: #{attribute.test_message}")
-                  f.puts("-"*120)
+                  f.puts("Child element test of attribute, Child attribute id: #{attribute.id}")
+                  f.puts("Child element test of attribute, Child attribute name: #{attribute.name}")
+                  f.puts("Child element test of attribute, Child attribute expected value: #{attribute.expected_value}")
+                  f.puts("Child element test of attribute, Child attribute actual value: #{attribute.actual_value}")
+                  f.puts("Child element test of attribute, Test status: #{attribute.test_status}")
+                  f.puts("Child element test of attribute, Test message: #{attribute.test_message}")
                 end
               end
+
+              unless child_element.test_message.nil?
+                f.puts(child_element.test_message)
+              end
             end
+            f.puts("*"*120)
+          end
+
+          unless element.test_message.nil?
+            f.puts(element.test_message)
           end
         end
         f.puts("")
         f.puts("")
-        f.puts("")
       end
+
+      @test_end_time = Time.now
+      @test_duration = @test_end_time - @test_start_time
+
+      f.puts "Test end time: #{@test_end_time}"
+      f.puts "Test duration: #{@test_duration}"
+      f.puts "Number of tested elements: #{@number_elements}"
     end
   end
 end
@@ -511,7 +595,6 @@ if (__FILE__ == $0)
     opt :api_key, "API key for access", :short => "-k", :type => :string, :default => "4bc231a2f0486a481425379be3093307"
     opt :channel, "Channel for test: all, movie, game, app, website, tv, show, book, music", :short => "-c", :type => :string, :default => "all"
     opt :limit, "Number of product for test: all or any number", :short => "-l", :type => :string, :default => '0'
-#    opt :input_file, "Relative path to the input xml file, instead of using remote server", :short => "-i", :type => :string, :default => ""
     opt :log_file_folder, "Relative to the log file", :type => :string, :default => default_log_directory
 
     opt :movie_element_spec, "Relative path to the movie element specification file", :type => :string, :default => default_movie_element_spec
@@ -528,7 +611,6 @@ if (__FILE__ == $0)
 
   # options validation
   Trollop::die :domain, "Domain name is missing" unless (opts[:domain])
-  #Trollop::die :input_file, "Input xml file (#{opts[:input_file]}) not found" unless (File.exists?(opts[:input_file]))
   Trollop::die :movie_element_spec, "test specification file (#{opts[:movie_element_spec]}) not found" unless (File.exists?(opts[:movie_element_spec]))
   Trollop::die :game_element_spec, "test specification file (#{opts[:game_element_spec]}) not found" unless (File.exists?(opts[:game_element_spec]))
   Trollop::die :app_element_spec, "test specification file (#{opts[:app_element_spec]}) not found" unless (File.exists?(opts[:app_element_spec]))
